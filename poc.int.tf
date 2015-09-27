c38a5c32d5f9c6a7e27d00b8328e8e0a95945e72
root <root@compass-monitoring-ci.emea1.cis.trcloud>
2015-09-27 16:36:56 +0000

    first commit

--- poc.int.tf
+++ poc.int.tf
@@ -1,10 +1,3 @@
-provider "openstack" {
-    user_name  = "8011868"
-    tenant_name = "F&R Platform Management Tools"
-    password  = "Infologo2006"
-    auth_url  = "http://identity-cis-trcloud.int.thomsonreuters.com:5000/v2.0"
-}
-
 variable "count" {
   default = 2
 }
---
+++ provider.int.tf
@@ -0,0 +1,6 @@
+provider "openstack" {
+    user_name  = "TO_REPLACE_USER_NAME"
+    tenant_name = "TO_REPLACE_TENANT_NAME"
+    password  = "TO_REPLACE_PASSWORD"
+    auth_url  = "TO_REPLACE_AUTH_URL"
+}
